cf8ce692655c89ee0cd1f2219472201740bf56d9
YueLu0116 <bit_drinkwater@163.com>
2023-02-26 14:46:09 +0800

    chore: organize the codes

--- texture/main.cpp
+++ texture/main.cpp
@@ -47,7 +47,7 @@ int main() {
     glViewport(0, 0, kWidth, kHeight);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    Shader our_shader("/Users/yuelu/develop/Graphics/LearnOpenGl/texture/texture.vs", "/Users/yuelu/develop/Graphics/LearnOpenGl/texture/texture.fs");
+    Shader our_shader("../texture_vs.glsl", "../texture_fs.glsl");
 
 
     float vertices[] = {
@@ -94,12 +94,13 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
     int width, height, nr_channels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load("/Users/yuelu/develop/Graphics/LearnOpenGl/texture/resource/messi.jpeg", &width, &height, &nr_channels, 0);
+    unsigned char *data = stbi_load("../resource/messi.jpeg",
+                                    &width, &height, &nr_channels, 0);
     if (data) {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
@@ -115,12 +116,13 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("/Users/yuelu/develop/Graphics/LearnOpenGl/texture/resource/awesomeface.png", &width, &height, &nr_channels, 0);
+    data = stbi_load("../resource/awesomeface.png", &width, &height, &nr_channels, 0);
     if (data) {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        // note that the awesomeface.png has transparency and thus an alpha channel,
+        // so make sure to tell OpenGL the data type is of GL_RGBA
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
