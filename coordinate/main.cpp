375af50977dfbe3c79b7252d6eafcc9542d87173
YueLu0116 <bit_drinkwater@163.com>
2023-03-18 20:51:29 +0800

    feat(coordinate): draw multiple cubes and make every 3rd container rotate over time

--- coordinate/main.cpp
+++ coordinate/main.cpp
@@ -58,8 +58,8 @@ int main()
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader("/Users/yuelu/develop/Graphics/LearnOpenGl/coordinate/trans.vs",
-                     "/Users/yuelu/develop/Graphics/LearnOpenGl/coordinate/trans.fs");
+    Shader ourShader("../trans_vs.glsl",
+                     "../trans_fs.glsl");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -139,7 +139,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load("/Users/yuelu/develop/Graphics/LearnOpenGl/texture/resource/messi.jpeg",
+    unsigned char *data = stbi_load("../../texture/resource/messi.jpeg",
                                     &width,
                                     &height,
                                     &nrChannels,
@@ -165,7 +165,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // load image, create texture and generate mipmaps
-    data = stbi_load("/Users/yuelu/develop/Graphics/LearnOpenGl/texture/resource/awesomeface.png",
+    data = stbi_load("../../texture/resource/awesomeface.png",
                      &width,
                      &height,
                      &nrChannels,
@@ -188,6 +188,19 @@ int main()
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
 
+    // more cubes
+    glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f),
+            glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3( 2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3( 1.3f, -2.0f, -2.5f),
+            glm::vec3( 1.5f,  2.0f, -2.5f),
+            glm::vec3( 1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
     // render loop
     // -----------
@@ -229,7 +242,21 @@ int main()
 
         // render box
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            if (i % 3 == 0){
+                angle = (float)glfwGetTime() * 25.0f;
+            }
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
